aaccfa2eb3bc727e8e1258b51a0ff12b2bdda1e1
Joshara Edwards <josharaedwards@yahoo.com>
2021-01-27 16:28:23 -0800

    Add project files.

---
+++ CS1C_Lab3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+
+	cout << endl << endl;
+	cout << "Press ENTER to exit program..." << endl;
+	cin.get();
+	return 0;
+}
