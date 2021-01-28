1b1266bff5fd502795eacea92754e49cdc0309c2
Andpa0520 <71115272+Andpa0520@users.noreply.github.com>
2021-01-27 19:11:29 -0800

    Update main.cpp

--- CS1C_Lab3/main.cpp
+++ CS1C_Lab3/main.cpp
@@ -1,15 +1,15 @@
 #include "func.h"
 
 //Joshara Edwards
-
+//Dayana Pulido
 
 int main()
 {
 	string name1 = "Joshara Edwards";
 	hello1Funtion(name1);
 
-	//string name2 = "name2";
-	//hello2Function(name2);
+	string name2 = "name2";
+	hello2Function(name2);
 
 	cout << endl << endl;
 	cout << "Press ENTER to exit program..." << endl;
